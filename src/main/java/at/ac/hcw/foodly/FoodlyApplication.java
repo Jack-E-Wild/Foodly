040cc95022f2463dae98dcb8949d4ef25ea3e3af
ViMar01 <viktoria.marlovits@gmail.com>
2026-06-06 22:22:25 +0200

    added an index.html for testing

--- src/main/java/at/ac/hcw/foodly/FoodlyApplication.java
+++ src/main/java/at/ac/hcw/foodly/FoodlyApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodlyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodlyApplication.class, args);
+		        SpringApplication.run(FoodlyApplication.class, args);
 	}
-
 }
