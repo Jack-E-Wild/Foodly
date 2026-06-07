c05bbebeb69d8185fcfa893f0ef5763f0cda5b62
ViMar01 <viktoria.marlovits@gmail.com>
2026-06-07 16:27:45 +0200

    added some more for testing

--- src/main/java/at/ac/hcw/foodly/controllers/HomeController.java
+++ src/main/java/at/ac/hcw/foodly/controllers/HomeController.java
@@ -1,12 +1,27 @@
 package at.ac.hcw.foodly.controllers;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
+
+    public String getIndex() {
         return "index.html";
+    }
+
+    @RequestMapping("/cooking")
+    @GetMapping
+    public String getCooking() {
+        return "cooking.html";
+    }
+    @DeleteMapping
+    public void deleteCooking() {
+        //ich könnte n delete handle sein
     }
 }
