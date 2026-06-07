ec4c4e7cbaf1c8588aee410dbd15c138967bad54
Tova Bele <tova.bele@stud.hcw.ac.at>
2026-06-07 21:24:23 +0200

    added foodgroup & ingredient constructors and test member

--- src/main/java/at/ac/hcw/foodly/models/FoodgroupModel.java
+++ src/main/java/at/ac/hcw/foodly/models/FoodgroupModel.java
@@ -1,5 +1,28 @@
 package at.ac.hcw.foodly.models;
 
 public class FoodgroupModel {
+    private String fgName;
+    private String fgIcon;
+
+    public FoodgroupModel(String fgName, String fgIcon) {
+
+    }
+
+    public void setFgName(String fgName) {
+        this.fgName = fgName;
+    }
+
+    public void setFgIcon(String fgIcon) {
+        this.fgIcon = fgIcon;
+    }
+
+    public String getFgName() {
+        return fgName;
+    }
+
+    public String getFgIcon() {
+        return fgIcon;
+    }
+
 
 }
--- src/main/java/at/ac/hcw/foodly/models/IngredientModel.java
+++ src/main/java/at/ac/hcw/foodly/models/IngredientModel.java
@@ -1,4 +1,55 @@
 package at.ac.hcw.foodly.models;
 
+import java.util.UUID;
+import java.util.List;
+import java.util.ArrayList;
+
 public class IngredientModel {
+    private UUID id;
+    private String ingrName;
+    private String fgName;
+    private String fgIcon;
+
+    public IngredientModel(){
+        this.id = UUID.randomUUID();
+    }
+
+    public IngredientModel(String ingrName, String fgName, String fgIcon) {
+        this();
+        this.ingrName = ingrName;
+        this.fgName = fgName;
+        this.fgIcon = fgIcon;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getIngrName() {
+        return ingrName;
+    }
+
+    public String getFgName() {
+        return fgName;
+    }
+
+    public String getFgIcon() {
+        return fgIcon;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setIngrName(String ingrName) {
+        this.ingrName = ingrName;
+    }
+
+    public void setFgName(String fgName) {
+        this.fgName = fgName;
+    }
+
+    public void setFgIcon(String fgIcon) {
+        this.fgIcon = fgIcon;
+    }
 }
