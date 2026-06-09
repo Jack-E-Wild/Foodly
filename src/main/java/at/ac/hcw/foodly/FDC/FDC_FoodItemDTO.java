5f056b523990a45ce7842fd2eabe8f9b30c1234f
Tova Bele <tova.bele@stud.hcw.ac.at>
2026-06-09 22:50:21 +0200

    added food data central search controller and data objects for spring boot

---
+++ src/main/java/at/ac/hcw/foodly/FDC/FDC_FoodItemDTO.java
@@ -0,0 +1,12 @@
+package at.ac.hcw.foodly.FDC;
+
+
+
+// @Data - needs lombok.Data
+// @AllArgsConstructor needs lombok.AllArgsConstructor
+public class FDC_FoodItemDTO {
+    private String foodItem;
+
+    public FDC_FoodItemDTO(String foodItem) {
+    }
+}
---
+++ src/main/java/at/ac/hcw/foodly/FDC/Query.java
@@ -0,0 +1,7 @@
+package at.ac.hcw.foodly.FDC;
+
+import org.springframework.http.ResponseEntity;
+
+public interface Query <I,O>{
+    ResponseEntity<O> execute (I input);
+}
