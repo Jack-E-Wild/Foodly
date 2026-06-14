ccf25f57a1592320287eeaa4c37431902c1ffd08
tova bele <tova.bele@stud.hcw.ac.at>
2026-06-15 00:42:43 +0200

    search endpoint reads header ? not working

--- src/main/java/at/ac/hcw/foodly/FDC/FDC_SearchController.java
+++ src/main/java/at/ac/hcw/foodly/FDC/FDC_SearchController.java
@@ -2,26 +2,30 @@ package at.ac.hcw.foodly.FDC;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 //purpose:
-    //1. send a query to the food API
-    //2. translate any reply into list of ingredient objects
-    //3. return list?
+//1. send a query to the food API
+//2. translate any reply into list of ingredient objects
+//3. return list?
 
 @RestController
-@RequestMapping("/api/public/search") //TODO: tova change this to /api/search once it works oder so
+@RequestMapping("/api/search")
 public class FDC_SearchController {
     @Autowired
     private FDC_QueryHandler fdcQueryHandler;
 
     @GetMapping
-    public ResponseEntity<List<FDC_FoodItemDTO>> getFoodItem(){
-        return fdcQueryHandler.execute("tuna");
+    public ResponseEntity<List<FDC_FoodItemDTO>> getFoodItem(
+            @RequestHeader(value = "X-Search-Term", required = false) String searchTerm) {
+
+        if (searchTerm == null || searchTerm.trim().isEmpty()) {
+            searchTerm = "";
+        }
+
+        return fdcQueryHandler.execute(searchTerm);
     }
 
     public FDC_SearchController() {
